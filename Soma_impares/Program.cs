8e3c0d66812524208015ebe3652c894eabaffa6b
DanLusBR <samu.nico837@gmail.com>
2026-04-23 13:40:03 -0300

    Soma_impares

---
+++ Soma_impares/Program.cs
@@ -0,0 +1,37 @@
+﻿// Soma dos impares
+// Fazer um programa para ler dois números inteiros e mostrar a soma dos números ímpares entre eles.
+
+﻿using System;
+
+namespace soma_impares
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x, y, soma, troca;
+
+			Console.WriteLine("Digite dois numeros:");
+			x = int.Parse(Console.ReadLine());
+			y = int.Parse(Console.ReadLine());
+
+			if (x > y)
+			{
+				troca = x;
+				x = y;
+				y = troca;
+			}
+
+			soma = 0;
+			for (int i = x + 1; i < y; i++)
+			{
+				if (i % 2 != 0)
+				{
+					soma = soma + i;
+				}
+			}
+
+			Console.WriteLine("SOMA DOS IMPARES = " + soma);
+		}
+	}
+}
